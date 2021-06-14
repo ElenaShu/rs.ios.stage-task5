763a81d00664e7a9e49622bd9daeb043dfc22b63
Elena <elen.shurigina@yandex.ru>
2021-06-14 22:16:05 +0300

    feat: add solution to the exercise-1

--- Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
+++ Swift_Task5/ios.stage-task/Exercises/Exercise-1/StockMaximize.swift
@@ -3,6 +3,26 @@ import Foundation
 class StockMaximize {
 
     func countProfit(prices: [Int]) -> Int {
-        return -1
+        var spent = 0
+        var spentCount = 0
+        var result = 0
+        if prices.count < 2 {
+            return 0
+        } else {
+            for i in 0..<(prices.count-1) {
+                if prices[i+1] < prices[i] {
+                    if spent > 0 {
+                        result += ((prices[i] * spentCount) - spent)
+                        spentCount = 0
+                        spent = 0
+                    }
+                } else {
+                    spent += prices[i]
+                    spentCount += 1
+                }
+            }
+            result += ((prices.last! * spentCount) - spent)
+            return result > 0 ? result : 0
+        }
     }
 }
